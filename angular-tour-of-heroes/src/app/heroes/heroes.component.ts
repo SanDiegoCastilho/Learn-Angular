9216dfa27d6f12cba48cc65254ec0777375badbc
San Diego Castilho <sandiegoocastilho@gmail.com>
2019-05-07 22:18:07 -0300

    Terminando editor de heroi

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,7 +9,8 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    f_name: 'Windstorm',
+    l_name: 'lero'
   };
 
   constructor() { }
